c6cb1fceb3701f6d63550a3d2afaf37f81a22b58
Muneera-Salah <muneera.salah@gmail.com>
2020-06-03 19:35:53 +0300

    implement artists function

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ db = SQLAlchemy(app)
 
 
 # TODO: connect to a local postgresql database
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -261,16 +261,7 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  data=Artist.query.all()
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
